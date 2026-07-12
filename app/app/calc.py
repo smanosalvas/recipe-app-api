47fd330802e288ac25d318c8b53a7f12daa2ca98
Stiven Manosalvas <Stiven.Manosalvas@zilliant.com>
2026-07-12 10:17:10 -0500

    Implemented command wait_for_db

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add two numbers and return the result."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract two numbers and return the result."""
+    return x - y
